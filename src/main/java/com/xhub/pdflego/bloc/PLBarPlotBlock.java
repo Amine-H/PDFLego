ee45b4a7d22faa75e214d905d969f5e6687508ad
Amine Hakkou <amine.hakkou.pro@gmail.com>
2016-10-04 17:07:33 +0100

    added Bar plot properties

--- src/main/java/com/xhub/pdflego/bloc/PLBarPlotBlock.java
+++ src/main/java/com/xhub/pdflego/bloc/PLBarPlotBlock.java
@@ -6,7 +6,26 @@ import com.xhub.pdflego.core.Component;
  * Created by amine
  */
 public class PLBarPlotBlock extends AbstractPlotBlock{
+    private double[] data;
+    private double barWidth;
+
     public PLBarPlotBlock(Component parent){
         super(parent);
+    }
+
+    public double[] getData() {
+        return data;
+    }
+
+    public void setData(double[] data) {
+        this.data = data;
+    }
+
+    public double getBarWidth() {
+        return barWidth;
+    }
+
+    public void setBarWidth(double barWidth) {
+        this.barWidth = barWidth;
     }
 }
