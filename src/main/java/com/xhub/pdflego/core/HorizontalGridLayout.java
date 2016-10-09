541e5ba717522c55cc2fde07017a7b255ab70ef4
Amine Hakkou <amine.hakkou.pro@gmail.com>
2016-10-09 23:17:16 +0100

    implemented dynamic Layouts with percentage

--- src/main/java/com/xhub/pdflego/core/HorizontalGridLayout.java
+++ src/main/java/com/xhub/pdflego/core/HorizontalGridLayout.java
@@ -1,7 +1,63 @@
 package com.xhub.pdflego.core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static javax.swing.UIManager.get;
+
 /**
- * Created by amine on 09/10/16.
+ * Created by amine
  */
-public class HorizontalGridLayout {
+public class HorizontalGridLayout extends Composite{
+    private List<Float> componentsSize = new ArrayList<>();
+
+    public HorizontalGridLayout(Component parent){
+        super(parent);
+        if(parent != null){
+            this.setHeight(parent.getHeight());
+            this.setWidth(parent.getWidth());
+        }
+    }
+
+    public void add(Component component, Float percentage){
+        componentsSize.add(percentage);
+        super.add(component);
+    }
+
+    @Override
+    public void postAdd(Component component) {
+        component.setHeight(this.getHeight());
+        this.calculateDimensions();
+    }
+
+    @Override
+    public void setWidth(Integer width){
+        super.setWidth(width);
+        this.calculateDimensions();
+    }
+
+    private void calculateDimensions(){
+        int components = super.childComponents.size();
+        int componentsSize = this.componentsSize.size();
+        if(components == componentsSize){
+            for(int i = 0;i < components;i++){
+                Float sizePercent = this.componentsSize.get(i);
+                Component component = super.childComponents.get(i);
+                component.setWidth(Math.round((sizePercent*this.getWidth())/100));
+                Integer offset = super.childComponents.subList(i, components).stream().map(Component::getWidth).mapToInt(w -> w).sum();
+                component.setX(offset);
+            }
+        }else{
+            //redistribute the size
+            this.componentsSize = (new ArrayList<>(components)).stream().map(gridSize -> (100f/components)).collect(Collectors.toList());
+            this.calculateDimensions();
+        }
+    }
+
+    @Override
+    public void preRemove(Component component) {
+        this.calculateDimensions();
+    }
 }
--- src/main/java/com/xhub/pdflego/core/VerticalGridLayout.java
+++ src/main/java/com/xhub/pdflego/core/VerticalGridLayout.java
@@ -1,7 +1,60 @@
 package com.xhub.pdflego.core;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- * Created by amine on 09/10/16.
+ * Created by amine
  */
-public class VerticalGridLayout {
+public class VerticalGridLayout extends Composite{
+    private List<Float> componentsSize = new ArrayList<>();
+
+    public VerticalGridLayout(Component parent){
+        super(parent);
+        if(parent != null){
+            this.setHeight(parent.getHeight());
+            this.setWidth(parent.getWidth());
+        }
+    }
+
+    public void add(Component component, Float percentage){
+        componentsSize.add(percentage);
+        super.add(component);
+    }
+
+    @Override
+    public void postAdd(Component component) {
+        component.setWidth(this.getWidth());
+        this.calculateDimensions();
+    }
+
+    @Override
+    public void setHeight(Integer height){
+        super.setHeight(height);
+        this.calculateDimensions();
+    }
+
+    private void calculateDimensions(){
+        int components = super.childComponents.size();
+        int componentsSize = this.componentsSize.size();
+        if(components == componentsSize){
+            for(int i = 0;i < components;i++){
+                Float sizePercent = this.componentsSize.get(i);
+                Component component = super.childComponents.get(i);
+                component.setHeight(Math.round((sizePercent*this.getHeight())/100));
+                Integer offset = super.childComponents.subList(i, components).stream().map(Component::getHeight).mapToInt(h -> h).sum();
+                component.setY(offset);
+            }
+        }else{
+            //redistribute the size
+            this.componentsSize = (new ArrayList<>(components)).stream().map(gridSize -> (100f/components)).collect(Collectors.toList());
+            this.calculateDimensions();
+        }
+    }
+
+    @Override
+    public void preRemove(Component component) {
+        this.calculateDimensions();
+    }
 }
